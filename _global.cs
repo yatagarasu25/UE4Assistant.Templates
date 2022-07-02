9559b97a275ca4b349500a1341466ff6f47e668f
Igor Karatayev <igor.cwer@gmail.com>
2022-07-02 20:46:26 +0300

    cleanup

--- _global.cs
+++ _global.cs
@@ -6,12 +6,3 @@ global using System.IO;
 global using System.Linq;
 global using System.Text;
 global using SystemEx;
-
-#if !NET5_0_OR_GREATER
-
-namespace System.Runtime.CompilerServices
-{
-	internal static class IsExternalInit { }
-}
-
-#endif
